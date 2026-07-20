f5f7281f7a63e63253bc71e18ce95a42c210b09c
danielvendzur-code <daniel.vendzur@gmail.com>
2026-07-20 23:25:20 +0200

    style: načítaj vertikálnu mobilnú optimalizáciu

--- src/components/site/Layout.tsx
+++ src/components/site/Layout.tsx
@@ -14,6 +14,7 @@ import "./LiquidChipPolish.css";
 import "./ChatbotChipRestore.css";
 import "./UnifiedExperience.css";
 import "./NativeWidgetPreview.css";
+import "./MobileVerticalPolish.css";
 
 const pageEase: [number, number, number, number] = [0.16, 1, 0.3, 1];
 
