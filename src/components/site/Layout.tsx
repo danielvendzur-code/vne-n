fb4e90752c1ed90b10db860e13521471825030b7
danielvendzur-code <daniel.vendzur@gmail.com>
2026-07-20 20:34:47 +0200

    style: načítaj finálny liquid chip systém

--- src/components/site/Layout.tsx
+++ src/components/site/Layout.tsx
@@ -9,6 +9,7 @@ import "./SitePolish.css";
 import "./MineralTheme.css";
 import "./MineralThemePolish.css";
 import "./WidgetMineral.css";
+import "./LiquidChipPolish.css";
 
 const pageEase: [number, number, number, number] = [0.16, 1, 0.3, 1];
 
