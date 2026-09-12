dbea14cce8b669f2839cfe130efcfd40cc37c5b2
danielvendzur-code <daniel.vendzur@gmail.com>
2026-09-12 19:49:34 +0200

    perf: remove global motion runtime from layout

--- src/components/site/Layout.tsx
+++ src/components/site/Layout.tsx
@@ -1,33 +1,31 @@
 import type { ReactNode } from "react";
 import { useRouterState } from "@tanstack/react-router";
-import { MotionConfig } from "motion/react";
 import { AnalyticsConsent } from "./AnalyticsConsent";
 import { Breadcrumbs } from "./Breadcrumbs";
 import { Nav } from "./Nav";
 import { Footer } from "./Footer";
 import { PageRevealController } from "./PageRevealController";
 import "./SiteVisualAuthority.css";
+import "./SubpageHeroUnified.css";
 
 export function SiteLayout({ children }: { children: ReactNode }) {
   const pathname = useRouterState({ select: (state) => state.location.pathname });
 
   return (
-    <MotionConfig reducedMotion="user">
-      <div className="site-theme-white-green min-h-screen flex flex-col">
-        <a className="skip-link" href="#main-content">
-          Preskočiť na obsah
-        </a>
-        <Nav />
-        <main id="main-content" className="relative flex-1">
-          <Breadcrumbs />
-          <div key={pathname} className="page-transition">
-            {children}
-          </div>
-          <PageRevealController pathname={pathname} />
-        </main>
-        <AnalyticsConsent />
-        <Footer />
-      </div>
-    </MotionConfig>
+    <div className="site-theme-white-green min-h-screen flex flex-col">
+      <a className="skip-link" href="#main-content">
+        Preskočiť na obsah
+      </a>
+      <Nav />
+      <main id="main-content" className="relative flex-1">
+        <Breadcrumbs />
+        <div key={pathname} className="page-transition">
+          {children}
+        </div>
+        <PageRevealController pathname={pathname} />
+      </main>
+      <AnalyticsConsent />
+      <Footer />
+    </div>
   );
 }
