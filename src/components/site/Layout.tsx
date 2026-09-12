5f649e686a4fc2a8ece807f8a7179fbd0245ccd9
danielvendzur-code <daniel.vendzur@gmail.com>
2026-09-12 19:50:17 +0200

    perf: remove layout-reading reveal controller from critical path

--- src/components/site/Layout.tsx
+++ src/components/site/Layout.tsx
@@ -1,16 +1,12 @@
 import type { ReactNode } from "react";
-import { useRouterState } from "@tanstack/react-router";
 import { AnalyticsConsent } from "./AnalyticsConsent";
 import { Breadcrumbs } from "./Breadcrumbs";
 import { Nav } from "./Nav";
 import { Footer } from "./Footer";
-import { PageRevealController } from "./PageRevealController";
 import "./SiteVisualAuthority.css";
 import "./SubpageHeroUnified.css";
 
 export function SiteLayout({ children }: { children: ReactNode }) {
-  const pathname = useRouterState({ select: (state) => state.location.pathname });
-
   return (
     <div className="site-theme-white-green min-h-screen flex flex-col">
       <a className="skip-link" href="#main-content">
@@ -19,10 +15,7 @@ export function SiteLayout({ children }: { children: ReactNode }) {
       <Nav />
       <main id="main-content" className="relative flex-1">
         <Breadcrumbs />
-        <div key={pathname} className="page-transition">
-          {children}
-        </div>
-        <PageRevealController pathname={pathname} />
+        <div className="page-transition">{children}</div>
       </main>
       <AnalyticsConsent />
       <Footer />
