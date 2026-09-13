d4f0b62b4eb3a1def4ca61256a5cd727a5618960
danielvendzur-code <daniel.vendzur@gmail.com>
2026-09-13 16:54:32 +0200

    Load configurator first-step discovery cue

--- src/components/SiteAssistantMount.tsx
+++ src/components/SiteAssistantMount.tsx
@@ -1,5 +1,6 @@
 import { ChameleonWidget } from "@/components/chatbot/ChameleonWidget";
 import "@/chatbot-widget.css";
+import "@/components/chatbot/FirstStepScrollCue.css";
 
 export function SiteAssistantMount() {
   return <ChameleonWidget />;
