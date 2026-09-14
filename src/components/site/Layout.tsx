14f5e159a99a7c3f3c1d25a817ba7e8be6b744ed
danielvendzur-code <daniel.vendzur@gmail.com>
2026-09-14 18:19:34 +0200

    Vráť nevydarené hover a process úpravy
    * Revert sep14 visual refinements from layout
    
    * Remove rejected sep14 visual override

--- src/components/site/Layout.tsx
+++ src/components/site/Layout.tsx
@@ -6,7 +6,6 @@ import { Breadcrumbs } from "./Breadcrumbs";
 import { Nav } from "./Nav";
 import { Footer } from "./Footer";
 import { PageRevealController } from "./PageRevealController";
-import { userRequestedSep14Css } from "./UserRequestedSep14";
 import "./SiteVisualAuthority.css";
 
 const navRefinement = `
@@ -79,7 +78,6 @@ export function SiteLayout({ children }: { children: ReactNode }) {
     <MotionConfig reducedMotion="user">
       <div className="site-theme-white-green min-h-screen flex flex-col">
         <style data-nav-refinement>{navRefinement}</style>
-        <style data-sep14-refinement>{userRequestedSep14Css}</style>
         <a className="skip-link" href="#main-content">
           Preskočiť na obsah
         </a>
